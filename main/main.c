c90bd3ef797c93079b41ded2b888fd827bb21ee0
Henrique Chaves <henrique.c-9@live.com.pt>
2023-05-26 12:09:21 +0100

    Update main

--- main/main.c
+++ main/main.c
@@ -6,44 +6,20 @@
 
 #include "esp_log.h"
 
-//#include "stepper_driver_tmc2208.h"
 #include "stepper_task.h"
 
-//#include "driver/rmt.h"
+static const char *TAG = "main";
 
-
-static const char* TAG = "main";
-
- 
 TaskHandle_t pvTask1 = NULL;
 TaskHandle_t pvTask2 = NULL;
 
-
-void app_main(void){
-
-    // Sparkfun Thing Plus - ESP32
-    /*
+void app_main(void)
+{
+    /* DevKits3  */
     static stepper_conf_t task1_conf = {
         .name = "Task 1",
         .speed = 100,
-        
-        .stepper_driver_conf.direction_pin = GPIO_NUM_12,
-        .stepper_driver_conf.step_pin = GPIO_NUM_23,
-        .stepper_driver_conf.enable_pin = GPIO_NUM_22,
-        .stepper_driver_conf.uart_port = UART_NUM_1,
-        .stepper_driver_conf.rx_pin = GPIO_NUM_16,
-        .stepper_driver_conf.tx_pin = GPIO_NUM_17,
-        .stepper_driver_conf.baud_rate = 115200,
-        .stepper_driver_conf.channel = RMT_CHANNEL_0,
-    };
-    */
 
-    // DevKits3
-    /**/
-    static stepper_conf_t task1_conf = {
-        .name = "Task 1",
-        .speed = 100,
-        
         .stepper_driver_conf.direction_pin = GPIO_NUM_12,
         .stepper_driver_conf.step_pin = GPIO_NUM_8,
         .stepper_driver_conf.enable_pin = GPIO_NUM_9,
@@ -57,7 +33,7 @@ void app_main(void){
     static stepper_conf_t task2_conf = {
         .name = "Task 2",
         .speed = 200,
-        
+
         .stepper_driver_conf.direction_pin = GPIO_NUM_4,
         .stepper_driver_conf.step_pin = GPIO_NUM_5,
         .stepper_driver_conf.enable_pin = GPIO_NUM_18,
@@ -69,11 +45,8 @@ void app_main(void){
     };
     */
     ESP_LOGI(TAG, "Starting Task 1");
-    //stepper_task(&task1_conf);
     xTaskCreatePinnedToCore(&stepper_task, "Task 1", 4096, &task1_conf, 5, &pvTask1, 1);
 
-
-    //ESP_LOGI(TAG, "Starting Task 2"); 
-    //xTaskCreatePinnedToCore(&stepper_task, "Task 2", 4096,  &task2_conf, 5, &pvTask2, 1);
-
+    // ESP_LOGI(TAG, "Starting Task 2");
+    // xTaskCreatePinnedToCore(&stepper_task, "Task 2", 4096,  &task2_conf, 5, &pvTask2, 1);
 }
--- main/stepper_task.c
+++ main/stepper_task.c
@@ -24,12 +24,10 @@ void stepper_task(void *pvParameter)
   char *task_name = config->name;
 
   ESP_LOGI(TAG, "%s: Init stepper driver", task_name);
-  // printf("Paraste Aqui Palhaço!! , %d",i);
 
   // Create new driver
   stepper_driver_t *motor = stepper_driver_new_tmc2208(&config->stepper_driver_conf);
 
-  ESP_LOGI(TAG, "%s: Driver created, start INIT", task_name);
   // Init driver
   stepper_driver_init(motor);
   // Clear status
@@ -40,7 +38,7 @@ void stepper_task(void *pvParameter)
   ESP_LOGI(TAG, "%s: Write settings to stepper driver", task_name);
 
   stepper_driver_set_stealthchop_thrs(motor, 0);
-  stepper_driver_set_current(motor, 1000, 50);
+  stepper_driver_set_current(motor, 600, 50);
   stepper_driver_set_microsteps_per_step(motor, MICROSTEPS_32);
 
   stepper_driver_enable_pwm_autograd(motor);
@@ -54,38 +52,9 @@ void stepper_task(void *pvParameter)
   stepper_driver_dump_register_drv_status(motor);
 
   ESP_LOGI(TAG, "%s: Move stepper per steps", task_name);
-  stepper_driver_direction(motor, 0);
 
-  for (int i = 0; i <= 400; i++)
+  for (int i = 0; i < 10; i++)
   {
-
-    printf("Measurement - %d\r\n", i);
-
-    /*
-    gpio_set_level(config->stepper_driver_conf.step_pin, 1);
-    vTaskDelay(pdMS_TO_TICKS(6000));
-    gpio_set_level(config->stepper_driver_conf.step_pin, 0);
-*/
-    stepper_driver_steps(motor, 8, config->speed);
-
-    vTaskDelay(pdMS_TO_TICKS(300));
-
-    // stepper_driver_direction(motor, 1);
-    // stepper_driver_steps(motor, 1000, config->speed * 2);
-
-    // vTaskDelay(pdMS_TO_TICKS(100));
-  }
-
-  stepper_driver_set_vactual(motor, -150);
-  vTaskDelay(pdMS_TO_TICKS(2000));
-  stepper_driver_set_vactual(motor, 0);
-  // stepper_driver_direction(motor, 1);
-  // stepper_driver_steps(motor, 1600, config->speed);
-
-  /*
-  ESP_LOGI(TAG, "%s: Move stepper per steps", task_name);
-
-  for (int i = 0; i < 10; i++) {
     stepper_driver_direction(motor, 0);
     stepper_driver_steps(motor, 1000, config->speed);
 
@@ -97,35 +66,19 @@ void stepper_task(void *pvParameter)
     vTaskDelay(pdMS_TO_TICKS(100));
   }
 
+  ESP_LOGD(TAG, "%s: Move stepper by register", task_name);
+  stepper_driver_set_vactual(motor, 10000);
 
-*/
-  /*
-   ESP_LOGI(TAG, "%s: Move stepper by register", task_name);
-   stepper_driver_set_vactual(motor, 500);
+  vTaskDelay(pdMS_TO_TICKS(2000));
 
-   vTaskDelay(pdMS_TO_TICKS(2000));
+  stepper_driver_set_vactual(motor, -10000);
 
-   stepper_driver_set_vactual(motor, -10000);
+  vTaskDelay(pdMS_TO_TICKS(2000));
 
-   vTaskDelay(pdMS_TO_TICKS(2000));
+  stepper_driver_set_vactual(motor, 0);
 
-   stepper_driver_set_vactual(motor, 0);
-
-
-   */
-
-  /* Disbale and Turn OFF motor*/
-
-  ESP_LOGI(TAG, "%s: Disable motor", task_name);
+  ESP_LOGD(TAG, "%s: Disable motor", task_name);
   stepper_driver_disable(motor);
 
   vTaskDelete(NULL);
-
-  /*
-    stepper_driver_set_pwm_grad(motor, 255);
-    stepper_driver_set_pwm_offset(motor, 255);
-    stepper_driver_disable_pwm_autograd(motor);
-    stepper_driver_disable_pwm_autoscale(motor);
-    stepper_driver_set_current(motor, 200, 100);
-  */
 }
